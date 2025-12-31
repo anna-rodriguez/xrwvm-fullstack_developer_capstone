a099c94f6856ad2ba56968d43072ac271b6b9f0e
anna czerwinska <68033924+anna-rodriguez@users.noreply.github.com>
2025-12-31 16:01:51 -0500

    Update urls.py

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -25,9 +25,20 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name="Home.html")),
     path('about/', TemplateView.as_view(template_name="About.html")),
     path('contact/', TemplateView.as_view(template_name="Contact.html")),
+    # add route for login
     path('login/', TemplateView.as_view(template_name="index.html")),
+    # add route for register
     path('register/', TemplateView.as_view(template_name="index.html")),
+    # add route for Dealers and Dealer
     path('dealers/', TemplateView.as_view(template_name="index.html")),
-    path('dealer/<int:dealer_id>',TemplateView.as_view(template_name="index.html")),
-    path('postreview/<int:dealer_id>',TemplateView.as_view(template_name="index.html")),
+    # add path for showing the dealer page
+    path(
+        'dealer/<int:dealer_id>',
+        TemplateView.as_view(template_name="index.html")
+    ),
+    # add path to post review page
+    path(
+        'postreview/<int:dealer_id>',
+        TemplateView.as_view(template_name="index.html")
+    ),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
