45be25f810a4834d0d859ed203a385a8b287ecbf
anna czerwinska <68033924+anna-rodriguez@users.noreply.github.com>
2025-12-31 15:46:54 -0500

    Update restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -5,44 +5,48 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-'backend_url', default="https://annarodrigue-3030.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/")
+backend_url = os.getenv(
+    'backend_url', default="http://localhost:3030")
 sentiment_analyzer_url = os.getenv(
     'sentiment_analyzer_url',
-    default="https://sentianalyzer.24kpzr3rew0h.us-south.codeengine.appdomain.cloud/")
+    default="http://localhost:5050/")
+
 
 def get_request(endpoint, **kwargs):
+    # Add code for get requests to back end
     params = ""
-    if(kwargs):
-        for key,value in kwargs.items():
-            params=params+key+"="+value+"&"
-
-    request_url = backend_url+endpoint+params
-    print("request url:", request_url)
+    if (kwargs):
+        for key, value in kwargs.items():
+            params = params + key + "=" + value + "&"
+    request_url = backend_url+endpoint+"?"+params
     print("GET from {} ".format(request_url))
     try:
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
         return response.json()
-    except:
+    except Exception:
         # If any error occurs
         print("Network exception occurred")
 
+
 def analyze_review_sentiments(text):
-    request_url = sentiment_analyzer_url+"/analyze/"+text
+    request_url = sentiment_analyzer_url+"analyze/"+text
+    # Add code for retrieving sentiments
     try:
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
         return response.json()
     except Exception as err:
-        print("Sentiment request_url:",request_url)
         print(f"Unexpected {err=}, {type(err)=}")
         print("Network exception occurred")
 
+
 def post_review(data_dict):
-    request_url = backend_url+"/insert_review"
+    # Add code for posting review
+    request_url = backend_url + "/insert_review"
     try:
-        response = requests.post(request_url,json=data_dict)
+        response = requests.post(request_url, json=data_dict)
         print(response.json())
         return response.json()
-    except:
+    except Exception:
         print("Network exception occurred")
