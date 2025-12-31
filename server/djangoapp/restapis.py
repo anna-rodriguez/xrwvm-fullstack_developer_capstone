d3bd6b6ac6e47d7ec76749f37688041963be53ee
anna-rodriguez <anna.rodriguez@ibm.com>
2025-12-31 17:27:40 -0500

    link updates

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -6,10 +6,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 backend_url = os.getenv(
-    'backend_url', default="http://localhost:3030")
+    'backend_url', default="https://annarodrigue-3030.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai")
 sentiment_analyzer_url = os.getenv(
     'sentiment_analyzer_url',
-    default="http://localhost:5050/")
+    default="https://sentianalyzer.24kl2ns2x1ex.us-south.codeengine.appdomain.cloud/")
 
 
 def get_request(endpoint, **kwargs):
